37e48ee90154521572e793ec5fdc6bc1fe66f7ce
julianay <bigpixel@gmail.com>
2015-01-06 11:46:08 -0500

    fixed problem with multi select

--- d3.parsets.js
+++ d3.parsets.js
@@ -5,7 +5,7 @@ var textRotation = 0;
 var tx = 10;
 var ty = 10;
 var parsetClicked = [];
-var clicked = false;
+var isSelected = false;
 var clickedNode = null;
 var chartWidth = 960;
 var chartHeight = 600;
@@ -302,17 +302,12 @@ var topCategories = /^(Best Actress|Best Actor|Best Supporting Actress|Best Supp
 
         // Unhighlight all nodes.
         function unhighlight() {
-          //if(parsetClicked) return;
           if (dragging) return;
           ribbon.classed("active", false);
           hideTooltip();
-          //remove null and create loop that only highlights stuff that has been clicked and exists in array
-          //parsetClicked.forEach(function(d){console.log("parset is: " + d.name)})
-          //console.log("parset is: " + parsetClicked[0].name);
           
-          //if(typeof parsetClicked !== undefined && parsetClicked.length !== null){
           if(parsetClicked.length > 0){	
-          	console.log("test new " + parsetClicked);
+          	//console.log("test new " + parsetClicked);
           	
           	for (var i = 0; i < parsetClicked.length; i++){
           		//console.log("parset is: " + parsetClicked[i].name);
@@ -320,15 +315,6 @@ var topCategories = /^(Best Actress|Best Actor|Best Supporting Actress|Best Supp
           	}
           	
           }
-          /*
-          if(parsetClicked == null){
-        		console.log("is null");
-        	}else{
-        		console.log("parset is: " + parsetClicked.name);
-        		//console.log();
-        		highlight(parsetClicked);
-        	}
-        	*/
         }
 
         function updateCategories(g) {
@@ -350,33 +336,32 @@ var topCategories = /^(Best Actress|Best Actor|Best Supporting Actress|Best Supp
               .on("mousedown.parsets", function(d) {
                
                	d.nodes.forEach(function(d) {
-               		console.log("highlight");
+               		//console.log("highlight");
+               		isSelected = false;
                		ribbon.classed("active", false);
                		highlight(d);
-               		//----- BUG IS HERE WITH THE FALSE/TRUE FOR CLICKED - CHECK IT
-               		//if(typeof parsetClicked !== undefined && parsetClicked !== null){
+               
                		if(parsetClicked.length > 0){
                			for (var i = 0; i < parsetClicked.length; i++){
-               				console.log("inside for cliked on " + d.name);
-               				console.log("inside for parsetClicked " + parsetClicked[i].name);
+               				//console.log("inside for cliked on " + d.name);
+               				//console.log("inside for parsetClicked " + parsetClicked[i].name);
                				if(d.name == parsetClicked[i].name){
-               					console.log("found: " + d.name);
-               					clicked = true;
+               					//console.log("found: " + d.name);
+               					//is this parset selected (found in the select list?) if found, isSelected = true
+               					isSelected = true;
                					if (i > -1) {
-               						console.log("splice: " + parsetClicked[i].name)
-               						//parsetClicked[i] = null;
     								parsetClicked.splice(i, 1);
 								}
                				}
           				}
-          			if(clicked == false){
-          				console.log("push: " + d.name);
+          			//has this parset been selected before? if it's not, add this parset
+          			if(isSelected == false){
+          				//console.log("push: " + d.name);
           				parsetClicked.push(d); 
           			}
-
-
+          			// is parsetClicked empty? push
                		}else{
-               			console.log(" EMPTY push: " + d.name);
+               			//console.log(" EMPTY push: " + d.name);
                			parsetClicked.push(d); 
                		}
                		
